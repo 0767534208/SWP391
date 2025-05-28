682ac7f609f0809405ac3292303092a39963ec5c
minhluan2510 <luanqmse182755@fpt.edu.vn>
2025-05-28 23:41:31 +0700

    Update UserLayout.tsx

--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function UserLayout() {
+  const location = useLocation();
+  const isAuthPage = location.pathname.includes('/auth/');
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
@@ -13,9 +16,11 @@ export default function UserLayout() {
 
       <Navbar/>
 
-      {/* Nội dung trang */}
+      {/* Main content with flex-grow to take all available space */}
+      <main className={`flex-grow ${isAuthPage ? 'flex flex-col' : ''}`}>
+        <Outlet /> {/* Display the child route */}
+      </main>
 
-        <Outlet /> {/* Hiển thị trang con */}
       <Footer/>
     </div>
   );
